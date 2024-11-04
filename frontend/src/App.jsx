9a566e44a0ab4a1753d2ce5aa9d743c741ee6092
Soham Barma <sohambarma483@gmail.com>
2024-11-04 12:55:37 +0530

    frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
-
-import './App.css'
-import navbar from './components/shared/navbar'
+import "./App.css";
+import Navbar from "./components/shared/navbar";
 
 function App() {
-  
-
   return (
     <>
-      <h1 className='text-red-500'>Lets built job portal</h1>
-      <navbar/>
+      <h1 className="text-red-500">Lets built job portal</h1>
+      <Navbar/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/shared/navbar.jsx
+++ frontend/src/components/shared/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <div className='bg-white'>
         <h1>Placement Tracker</h1>
@@ -8,4 +8,4 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default Navbar
