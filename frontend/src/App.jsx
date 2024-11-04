dd262178eda6c96070ff62ef4839af080b721254
TURJO DAS <turjo.das2001@gmail.com>
2024-11-04 12:00:31 +0530

    navbar check

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import navbar from './components/shared/navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-red-500'>Lets built job portal</h1>
+      <navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/shared/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='bg-white'>
+        <h1>Placement Tracker</h1>
+    </div>
+  )
+}
+
+export default navbar
